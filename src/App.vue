845057713b414b56f8786d59184bb423489ede6b
guan <635344988@qq.com>
2021-06-25 23:27:24 +0800

    描述

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      show: true,
+      url: 'https://www.github.com',
+      number: 1,
+      books: [
+        {name:'vue/js action'},
+        {name:'javascript la'}
+      ]
+    }
+  },
+  computed: {
+    atest: function() {
+      console.log("do atest");
+      return this.number - 1
+    }
+  },
+  methods: {
+    handleClose: function () {
+      this.show = false
+    },
+    calcute: function () {
+      console.log("do calcute");
+      return this.number - 1
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import LOGIN from '../view/login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: LOGIN,
+      meta: {
+        title: '登录页'
+      },
+    },
+    // { path: '/', redirect: '/login' },
+    // { path: '/login', component: LOGIN }
+  ]
+})
