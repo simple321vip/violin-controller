3a7b29e3db2f2c9a9f5e2a807a72c3de5e4cd738
guan <635344988@qq.com>
2021-06-26 23:15:54 +0800

    layout　サンプル

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LOGIN from '../view/login'
+import layout from '../components/Layout'
 
 Vue.use(Router)
 
@@ -13,6 +14,11 @@ export default new Router({
       meta: {
         title: '登录页'
       }
+    },
+    {
+      path: '/',
+      name: 'layout',
+      component: layout
     }
     // { path: '/', redirect: '/login' },
     // { path: '/login', component: LOGIN }
